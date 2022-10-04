cfa80f0e5de72003321beeae48b54f1eb9f1cfae
Caleb Ockwig <31769782+CJOckwig@users.noreply.github.com>
2022-10-04 10:52:45 -0500

    Add files via upload

---
+++ ExOckwig/ExOckwig/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using ExOckwig;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        String FilePath = (args.Length == 1) ? args[0] : null;
+        BST SymbolTable = new BST();
+        BST.LeftTraverse(SymbolTable.Root);
+        LinkedList<Literal> LiteralTable; 
+        ExVal expression = new ExVal(FilePath, out LiteralTable, SymbolTable);
+
+        Console.WriteLine("Literal    Value      Size   Address");
+        foreach (Literal Lit in LiteralTable)
+        {
+            Console.WriteLine(Lit.ToString());
+        }
+    }
+}
